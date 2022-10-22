22680e7ac52e1228adc9ab385e8020f87e441676
Samet Dogan <Samet.d421@gmail.com>
2022-10-22 18:31:04 +0200

    init

---
+++ src/main/java/de/htwberlin/webtechproject/WebtechprojectApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtechproject/WebtechprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
